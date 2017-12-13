969c8ca19dbe83065efb58317f15327bb6e06fd4
Herasym <OHerasym@kiev.luxoft.com>
2017-12-13 16:27:49 +0200

    add config

---
+++ config.py
@@ -0,0 +1,9 @@
+
+
+url_repository = 'https://jazz.kiev.luxoft.com:9443/ccm/'
+
+sources_dir = 'D:\\test_build'
+
+scm_path = 'D:\\Eclipse452_PF_RTC602_Win64\\Eclipse452_PF_RTC602_Win64\\scmtools\\eclipse\\scm.exe'
+
+default_repository = 'local'
